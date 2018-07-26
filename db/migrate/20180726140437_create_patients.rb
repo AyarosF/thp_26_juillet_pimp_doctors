519c3e82a1d85a95a72dd25c1f56ec0cf94f6779
AyarosF <sorayafructuoso@gmail.com>
2018-07-27 01:34:54 +0200

    ajout comms

--- db/migrate/20180726140437_create_patients.rb
+++ db/migrate/20180726140437_create_patients.rb
@@ -3,6 +3,7 @@ class CreatePatients < ActiveRecord::Migration[5.2]
     create_table :patients do |t|
       t.string :first_name
       t.string :last_name
+      # ajout city_id table patients 
       t.belongs_to :city, index: true
 
       t.timestamps
--- db/migrate/20180726140524_create_appointments.rb
+++ db/migrate/20180726140524_create_appointments.rb
@@ -2,6 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[5.2]
   def change
     create_table :appointments do |t|
       t.datetime :date
+      # doctor_id, patient_id et city_id dans table appointments 
       t.references :doctor, foreign_key: true
       t.references :patient, foreign_key: true
       t.belongs_to :city, index: true
